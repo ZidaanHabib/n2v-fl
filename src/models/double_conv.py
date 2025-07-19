955690f11ca7d64196c6ee5af0bbcd7be4fabe08
Zidaan <zizouhabib@gmail.com>
2025-07-19 21:25:19 +0100

    feat: add unet model

---
+++ src/models/double_conv.py
@@ -0,0 +1,18 @@
+import torch
+from torch import nn
+
+class DoubleConv(nn.Module):
+
+    def __init__(self, in_channels, out_channels):
+        super().__init__()
+        
+        self.net  = nn.Sequential(
+            nn.Conv2d(in_channels=in_channels,out_channels=out_channels,kernel_size=3,stride=1,padding=1),
+            nn.BatchNorm2d(out_channels),
+            nn.Relu(),
+            nn.Conv2d(out_channels, out_channels,kernel_size=3,stride=1, padding=1),
+            nn.BatchNorm2d(32),
+            nn.Relu(),
+        )           
+    def forward(self, x: torch.Tensor):
+        return self.net(x)     
