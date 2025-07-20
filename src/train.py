fa869973a967e89829d10f1282550aa023f584c3
Zidaan <zizouhabib@gmail.com>
2025-07-20 02:44:14 +0100

    fix: correct dict key

--- src/train.py
+++ src/train.py
@@ -5,8 +5,7 @@ from utils.build import load_dataset
 
 @hydra.main(version_base=None, config_path="config", config_name="default")
 def main(cfg: DictConfig):
-    train_loader, test_loader = load_dataset(batch_size=cfg.data.batches, num_workers=cfg.data.num_workers, patch_size=cfg.data.patch_size)
-    
+    train_loader, test_loader = load_dataset(batch_size=cfg.data.batch_size, num_workers=cfg.data.num_workers, patch_size=cfg.data.patch_size)
 
 if __name__ == "__main__":
     main()
