9693380cd6c768a615dccecf321e7251354b7b36
Zidaan <zizouhabib@gmail.com>
2025-07-20 02:38:36 +0100

    feat: load datasets into dataloader

--- src/train.py
+++ src/train.py
@@ -5,9 +5,8 @@ from utils.build import load_dataset
 
 @hydra.main(version_base=None, config_path="config", config_name="default")
 def main(cfg: DictConfig):
-    dataset = load_dataset(batch_size=16, num_workers=0, patch_size=256 )
-    print(len(dataset))
-
+    train_loader, test_loader = load_dataset(batch_size=cfg.data.batches, num_workers=cfg.data.num_workers, patch_size=cfg.data.patch_size)
+    
 
 if __name__ == "__main__":
     main()
--- src/utils/build.py
+++ src/utils/build.py
@@ -1,6 +1,6 @@
 
 from torch.nn import functional as F    
-from torch.utils.data import DataLoader, Dataset, ConcatDataset
+from torch.utils.data import DataLoader, ConcatDataset, random_split
 from torchvision.transforms import v2
 from pathlib import Path
 
@@ -25,8 +25,6 @@ def load_dataset(batch_size: int, num_workers = 0, patch_size = 256 ):
     ])
 
     base_dir = Path().resolve().parent
-    print(base_dir)
-    # tst = N2NImageDataset(base_dir, dataset="20x-noise1", subdataset="actin-20x-noise1",transform=transform, patches_per_image=256)
 
     subdatasets = [ N2NImageDataset(base_dir, dataset="20x-noise1", subdataset="actin-20x-noise1",transform=transform, patches_per_image=16),
                 N2NImageDataset(base_dir, dataset="20x-noise1", subdataset="mito-20x-noise1",transform=transform, patches_per_image=16),
@@ -39,4 +37,25 @@ def load_dataset(batch_size: int, num_workers = 0, patch_size = 256 ):
                 N2NImageDataset(base_dir, dataset="membrane", subdataset="membrane",transform=transform, patches_per_image=16),
                 N2NImageDataset(base_dir, dataset="nucleus", subdataset="nucleus",transform=transform_nucleus, patches_per_image=16)
     ]
-    return ConcatDataset(subdatasets)
+    dataset = ConcatDataset(subdatasets)
+    dataset_length = len(dataset)
+    train_size = int(0.8 * dataset_length)
+    test_size = dataset_length - train_size
+
+    train_dataset, test_dataset = random_split(dataset, (train_size,test_size))
+
+    train_loader = DataLoader(
+        train_dataset,
+        batch_size=batch_size,
+        shuffle=True,
+        num_workers=num_workers,
+        pin_memory=True
+    )
+    test_loader = DataLoader(
+        test_dataset,
+        batch_size=batch_size,
+        shuffle=True,
+        num_workers=num_workers,
+        pin_memory=True
+    )
+    return train_loader, test_loader
