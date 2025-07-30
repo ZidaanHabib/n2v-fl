cdc0ccae58e0f531b32540d41b3892b76a730f07
Zidaan <zizouhabib@gmail.com>
2025-07-30 19:36:51 +0100

    feat: add training/test functionality for the synthetic dataset

--- src/train.py
+++ src/train.py
@@ -4,7 +4,7 @@ import hydra
 from omegaconf import DictConfig
 
 
-from utils.build import load_dataset, load_distributed_dataset, setup_loss, setup_optimizer, train_step, test_step, seed, set_device
+from utils.build import load_dataset, load_distributed_dataset, setup_loss, setup_optimizer, test_step_synthetic, train_step, test_step, seed, set_device, train_step_synthetic
 from models.unet import UNet
 
 import torch
@@ -33,6 +33,8 @@ def main(cfg: DictConfig):
     #instantiate model
     model = UNet(1,cfg.model.base_channels, cfg.model.depth, batch_norm=cfg.model.batch_norm).to(local_rank)
 
+    has_ground_truth = cfg.data.has_ground_truth
+
     # set up paths for data access
     root = Path().resolve().parent
     data_dir = Path(cfg.data.data_dir) if cfg.data.data_dir else root / "data" / "preprocessed"
@@ -44,7 +46,7 @@ def main(cfg: DictConfig):
         # wrap model in DDP for distributed processing
         model = DDP(model,device_ids=[local_rank], output_device=local_rank)
         #load dataset:
-        train_loader, test_loader = load_distributed_dataset(rank=rank, world_size=world_size, data_dir=data_dir, batch_size=cfg.data.batch_size, num_workers=cfg.data.num_workers, patch_size=cfg.data.patch_size, patches_per_image=cfg.data.patches_per_image, seed=seed_value)
+        train_loader, test_loader = load_distributed_dataset(rank=rank, world_size=world_size, data_dir=data_dir, batch_size=cfg.data.batch_size, num_workers=cfg.data.num_workers, patch_size=cfg.data.patch_size, patches_per_image=cfg.data.patches_per_image, seed=seed_value, has_ground_truth=has_ground_truth)
     else:
         train_loader, test_loader = load_dataset(data_dir=data_dir, batch_size=cfg.data.batch_size, num_workers=cfg.data.num_workers, patch_size=cfg.data.patch_size, patches_per_image=cfg.data.patches_per_image, seed=seed_value)
 
@@ -61,12 +63,24 @@ def main(cfg: DictConfig):
 
     epochs = int(cfg.train.epochs)
 
+
+    
+
+    dist.barrier() # synchronise processes before starting timer
+    start_time = time.perf_counter()
+    if has_ground_truth:
+        perform_synthetic_model_training(rank, device, model,train_loader, test_loader, loss_fn, optim, cluster_run_dir_name, epochs, start_time)
+    else:
+        perform_model_training(rank, device, model, train_loader, test_loader, loss_fn, optim, cluster_run_dir_name, epochs, start_time)
+
+    dist.destroy_process_group()
+
+def perform_model_training(rank, device, model, train_loader, test_loader, loss_fn, optim, cluster_run_dir_name, epochs, start_time):
+
     best_val_loss = torch.tensor(float("inf"), device=device, requires_grad=False)
     avg_train_losses = torch.zeros(epochs, device=device, requires_grad=False) 
     avg_test_losses =  torch.zeros(epochs, device=device, requires_grad=False)
 
-    dist.barrier() # synchronise processes before starting timer
-    start_time = time.perf_counter()
     for epoch in range(1,epochs+1):
         if rank == 0:
             print(f"Epoch: {epoch}-----------------------------------------\n\n")
@@ -78,7 +92,7 @@ def main(cfg: DictConfig):
             device=device,
             epoch=epoch,
             rank=rank,
-            dir_name=cluster_run_dir_name
+            dir_name=cluster_run_dir_name,
         )
 
         avg_test_loss = test_step(data_loader=test_loader,
@@ -87,7 +101,7 @@ def main(cfg: DictConfig):
             device=device,
             epoch=epoch,
             rank=rank,
-            dir_name=cluster_run_dir_name
+            dir_name=cluster_run_dir_name,
         )
         
         # Reduce train loss across different processes
@@ -100,7 +114,7 @@ def main(cfg: DictConfig):
             avg_train_losses[epoch - 1] = avg_train_loss
             # append epoch loss to running list:
             avg_test_losses[epoch - 1] = avg_test_loss
-               
+            
             torch.save(
                 {
                     "epoch":       epoch,
@@ -123,7 +137,7 @@ def main(cfg: DictConfig):
                     },
                     f"runs/{cluster_run_dir_name}/checkpoints/best.pth",
                 )
-    
+        
     dist.barrier()
     total_time = time.perf_counter() - start_time
     if rank == 0:
@@ -133,9 +147,97 @@ def main(cfg: DictConfig):
             f.write("Train_loss,Test_loss\n")
             f.writelines(f"{train_loss.item()},{test_loss.item()}\n" for (train_loss, test_loss) in zip(avg_train_losses, avg_test_losses))
         print("Done")
-
-    dist.destroy_process_group()
     
+def perform_synthetic_model_training(rank, device, model, train_loader, test_loader, loss_fn, optim, cluster_run_dir_name, epochs, start_time):
+    #Initialise
+    best_val_loss = torch.tensor(float("inf"), device=device, requires_grad=False)
+    avg_train_losses = []
+    avg_test_losses =  []
+    avg_train_psnr_values = []
+    avg_test_psnr_values = []
+
+    for epoch in range(1,epochs+1):
+        if rank == 0:
+            print(f"Epoch: {epoch}-----------------------------------------\n\n")
+        
+        avg_train_loss, avg_train_psnr = train_step_synthetic(data_loader=train_loader, 
+            model=model, 
+            loss_fn=loss_fn,
+            opt=optim,
+            device=device,
+            epoch=epoch,
+            rank=rank,
+            dir_name=cluster_run_dir_name,
+        )
+
+        avg_test_loss, avg_test_psnr = test_step_synthetic(data_loader=test_loader,
+            model=model,
+            loss_fn=loss_fn,
+            device=device,
+            epoch=epoch,
+            rank=rank,
+            dir_name=cluster_run_dir_name,
+        )
+        
+        # Reduce train loss across different processes
+        dist.all_reduce(avg_train_loss, op=dist.ReduceOp.AVG)
+        dist.all_reduce(avg_test_loss,op=dist.ReduceOp.AVG)
+        
+        # Reduce psnr across different processes
+        dist.all_reduce(avg_train_psnr, op=dist.ReduceOp.AVG)
+        dist.all_reduce(avg_test_psnr, op=dist.ReduceOp.AVG)
+
+
+        if rank == 0:
+            # append epoch loss to running list: 
+            avg_train_losses.append(avg_train_loss.item())
+            # append epoch loss to running list:
+            avg_test_losses.append(avg_test_loss.item())
+            # append epoch psnr to running list:
+            avg_train_psnr_values.append(avg_train_psnr.item())
+            # append epoch psnr to running list:
+            avg_test_psnr_values.append(avg_test_psnr.item())         
+            torch.save(
+                {
+                    "epoch":       epoch,
+                    "model_state": model.state_dict(),
+                    "optim_state": optim.state_dict(),
+                    "avg_train_loss": avg_train_loss,
+                    "avg_test_loss":    avg_test_loss,
+                    "avg_train_psnr": avg_train_psnr,
+                    "avg_test_psnr": avg_test_psnr
+                },
+                f"runs/{cluster_run_dir_name}/checkpoints/last.pth",
+            )
+
+            # If it’s the best so far, also save “best.pth”
+            if avg_test_loss < best_val_loss:
+                best_val_loss = avg_test_loss
+                torch.save(
+                    {
+                        "model_state": model.state_dict(),  # you can omit optimizer if only inference
+                        "avg_test_loss": avg_test_loss,
+                        "avg_train_loss": avg_train_loss,
+                        "avg_train_psnr": avg_train_psnr,
+                        "avg_test_psnr": avg_test_psnr
+                    },
+                    f"runs/{cluster_run_dir_name}/checkpoints/best.pth",
+                )
+        
+    dist.barrier()
+    total_time = time.perf_counter() - start_time
+    if rank == 0:
+        print(f"Training time: {(total_time / 60):.2f} minutes")
+        print("Writing avg epoch losses to file...")
+        with open(f"runs/{cluster_run_dir_name}/output/epoch_losses.txt", "w") as f:
+            f.write("Train_loss,Test_loss\n")
+            f.writelines(f"{train_loss},{test_loss}\n" for (train_loss, test_loss) in zip(avg_train_losses, avg_test_losses))
+        print("Done")
+        print("Writing avg epoch psnr values to file...")
+        with open(f"runs/{cluster_run_dir_name}/output/epoch_psnr_values.txt", "w") as f:
+            f.write("Train_psnr,Test_psnr\n")
+            f.writelines(f"{train_psnr},{test_psnr}\n" for (train_psnr, test_psnr) in zip(avg_train_psnr_values, avg_test_psnr_values))
+        print("Done")
 
 if __name__ == "__main__":
     main()
--- src/utils/build.py
+++ src/utils/build.py
@@ -7,7 +7,7 @@ from torch.utils.data import DataLoader, ConcatDataset, random_split, Distribute
 from torchvision.transforms import v2
 from pathlib import Path
 
-from data.dataset import N2NImageDataset
+from data.dataset import N2NImageDataset, N2NSyntheticImageDataset
 
 # define transforms for dataset
 transform_confocal = v2.Compose([
@@ -111,8 +111,23 @@ def  create_dataset(data_dir, patch_size, patches_per_image) :
     dataset = ConcatDataset(subdatasets)
     return dataset
 
-def load_distributed_dataset(world_size: int, rank: int, data_dir: Path, batch_size: int, num_workers: int, patch_size: int, patches_per_image: int, seed: int = 42):
-    dataset = create_dataset(data_dir, patch_size, patches_per_image)
+def  create_synthetic_dataset(data_dir, patch_size, patches_per_image) :
+    subdatasets = [ N2NSyntheticImageDataset(data_dir, dataset="20x-noise1", subdataset="actin-20x-noise1",transform=transform, patch_size=patch_size, patches_per_image=patches_per_image),
+                N2NSyntheticImageDataset(data_dir, dataset="20x-noise1", subdataset="mito-20x-noise1",transform=transform, patch_size=patch_size, patches_per_image=patches_per_image),
+                N2NSyntheticImageDataset(data_dir, dataset="60x-noise1", subdataset="actin-60x-noise1",transform=transform, patch_size=patch_size, patches_per_image=patches_per_image),
+                N2NSyntheticImageDataset(data_dir, dataset="60x-noise1", subdataset="mito-60x-noise1",transform=transform, patch_size=patch_size, patches_per_image=patches_per_image),
+                N2NSyntheticImageDataset(data_dir, dataset="60x-noise2", subdataset="actin-60x-noise2",transform=transform, patch_size=patch_size, patches_per_image=patches_per_image),
+                N2NSyntheticImageDataset(data_dir, dataset="60x-noise2", subdataset="mito-60x-noise2",transform=transform, patch_size=patch_size, patches_per_image=patches_per_image),
+                N2NSyntheticImageDataset(data_dir, dataset="confocal", subdataset="actin-confocal",transform=transform_confocal, patch_size=patch_size, patches_per_image=patches_per_image),
+                N2NSyntheticImageDataset(data_dir, dataset="confocal", subdataset="mito-confocal",transform=transform_confocal, patch_size=patch_size, patches_per_image=patches_per_image),
+                N2NSyntheticImageDataset(data_dir, dataset="membrane", subdataset="membrane",transform=transform, patch_size=patch_size, patches_per_image=patches_per_image),
+                N2NSyntheticImageDataset(data_dir, dataset="nucleus", subdataset="nucleus",transform=transform_nucleus, patch_size=patch_size, patches_per_image=patches_per_image)
+    ]
+    dataset = ConcatDataset(subdatasets)
+    return dataset
+
+def load_distributed_dataset(world_size: int, rank: int, data_dir: Path, batch_size: int, num_workers: int, patch_size: int, patches_per_image: int, seed: int = 42, has_ground_truth: bool = False):
+    dataset = create_synthetic_dataset(data_dir,patch_size, patches_per_image) if has_ground_truth else create_dataset(data_dir, patch_size, patches_per_image)
     dataset_length = len(dataset)
     train_size = int(0.9 * dataset_length)
     test_size = dataset_length - train_size
@@ -168,6 +183,7 @@ def train_step(model, data_loader, loss_fn, opt, device, epoch, rank, dir_name: 
     running_loss = torch.tensor(0, dtype=torch.float32, device=device, requires_grad=False)
     losses = torch.zeros(len(data_loader),device=device,requires_grad=False) 
 
+
     for batch, (X, y) in enumerate(data_loader):
         
         X, y = X.to(device), y.to(device)
@@ -183,24 +199,69 @@ def train_step(model, data_loader, loss_fn, opt, device, epoch, rank, dir_name: 
         losses[batch] = current_loss
         running_loss   += current_loss
 
-        # with torch.no_grad():
-        #     # if your data range is [0,1]; otherwise pass max_val=255
-        #     batch_psnr = compute_psnr(denoised, y, max_val=1.0)
-        # running_psnr += batch_psnr
         print(f"Rank {rank} | Batch {batch} done | Batch loss: {current_loss:.5f}")
- 
+    
 
     dist.all_reduce(losses, op=dist.ReduceOp.AVG)
-    if epoch % 20 == 0 or epoch == 1:
-        with open(f"runs/{dir_name}/output/epoch_{epoch}_train_losses.txt", "w") as f:
-            f.write(f"Epoch {epoch}\n")
-            f.writelines(f"{loss.item():.5f}\n" for loss in losses)
+    if rank == 0:
+        if epoch % 20 == 0 or epoch == 1:
+            with open(f"runs/{dir_name}/output/epoch_{epoch}_train_losses.txt", "w") as f:
+                f.write(f"Epoch {epoch}\n")
+                f.writelines(f"{loss.item():.5f}\n" for loss in losses)
 
     rank_epoch_loss   = running_loss   / len(data_loader)
     # epoch_psnr = running_psnr/ len(data_loader)
 
     print(f"Rank {rank} | Train loss: {rank_epoch_loss:.5f} ")
     return rank_epoch_loss
+
+def train_step_synthetic(model, data_loader, loss_fn, opt, device, epoch, rank, dir_name: str) -> Tuple[torch.Tensor, torch.Tensor] :
+    model.train()
+    running_loss = torch.tensor(0, dtype=torch.float32, device=device, requires_grad=False)
+    losses = torch.zeros(len(data_loader),device=device,requires_grad=False) 
+    running_psnr = torch.tensor(0, dtype=torch.float32, device=device, requires_grad=False)
+    psnr_list = torch.zeros(len(data_loader), device=device, requires_grad=False)
+
+
+    for batch, (X, y, clean) in enumerate(data_loader):
+        
+        X, y, clean = X.to(device), y.to(device), clean.to(device)
+
+        opt.zero_grad()
+        denoised = model(X)
+        loss   = loss_fn(denoised, y)
+
+        loss.backward()
+        opt.step()
+
+        current_loss = loss.item()
+        losses[batch] = current_loss
+        running_loss   += current_loss
+
+        with torch.inference_mode():
+            current_psnr = compute_psnr(denoised, y, clean, 1.0).item()
+        psnr_list[batch] = current_psnr
+        running_psnr += current_psnr
+
+        print(f"Rank {rank} | Batch {batch} done | Batch loss: {current_loss:.5f} | Batch PSNR: {current_psnr}")
+    
+
+    dist.all_reduce(losses, op=dist.ReduceOp.AVG)
+    dist.all_reduce(psnr_list, op=dist.ReduceOp.AVG)
+    if rank == 0:
+        if epoch % 20 == 0 or epoch == 1:
+            with open(f"runs/{dir_name}/output/epoch_{epoch}_train_losses.txt", "w") as f:
+                f.write(f"Epoch {epoch}\n")
+                f.writelines(f"{loss.item():.5f}\n" for loss in losses)
+            with open(f"runs/{dir_name}/output/epoch_{epoch}_train_psnr.txt", "w") as f:
+                f.write(f"Epoch {epoch}\n")
+                f.writelines(f"{psnr.item():.2f}\n" for psnr in psnr_list)
+
+    rank_epoch_loss   = running_loss   / len(data_loader)
+    rank_epoch_psnr = running_psnr/ len(data_loader)
+
+    print(f"Rank {rank} | Epoch {epoch} | Avg Epoch Train loss: {rank_epoch_loss:.5f} | Avg Epoch Train PSNR: {rank_epoch_psnr:.2f} ")
+    return rank_epoch_loss, rank_epoch_psnr
 
 def test_step(
     model: torch.nn.Module,
@@ -252,7 +313,62 @@ def test_step(
     print(f"Rank {rank} | Test loss: {rank_avg_loss:.5f} ")
     return rank_avg_loss
 
-def compute_psnr(pred, target, max_val=1.0):
+def test_step_synthetic(
+    model: torch.nn.Module,
+    data_loader: torch.utils.data.DataLoader,
+    loss_fn: torch.nn.Module,
+    device: torch.device,
+    epoch: int,
+    rank: int,
+    dir_name: str
+) -> Tuple[torch.Tensor, torch.Tensor] :
+    # switch to eval mode
+    model.eval()
+
+    total_loss = torch.tensor(0, dtype=torch.float32, device=device, requires_grad=False)
+    total_psnr  = torch.tensor(0, dtype=torch.float32, device=device, requires_grad=False)
+    losses = torch.zeros(len(data_loader), device=device, requires_grad=False)
+    psnr_list = torch.zeros(len(data_loader), device=device, requires_grad=False)
+    # No gradients needed
+    with torch.inference_mode():
+        for batch, (X, y, clean) in enumerate(data_loader):
+            # Send data to the same device
+            X, y = X.to(device), y.to(device)
+
+            # Forward pass
+            denoised = model(X)
+
+            # Compute loss & metric (use .item() to get floats)
+            loss = loss_fn(denoised, y)
+            current_loss = loss.item()
+            losses[batch] = current_loss
+            total_loss += current_loss
+
+            batch_psnr = compute_psnr(denoised, y, clean, max_val=1.0).item()
+            psnr_list[batch] = batch_psnr
+            total_psnr += batch_psnr
+
+            print(f"Rank {rank} | Validation Batch {batch} done | Validation batch loss: {current_loss:.5f} | PSNR: {batch_psnr}")
+
+    dist.all_reduce(losses, op=dist.ReduceOp.AVG)
+    dist.all_reduce(psnr_list, op=dist.ReduceOp.AVG)
+    if rank  == 0:
+        if epoch % 20 == 0 or epoch == 1:
+            with open(f"runs/{dir_name}/output/epoch_{epoch}_test_losses.txt", "w") as f:
+                f.write(f"Epoch {epoch}\n")
+                f.writelines(f"{loss.item():.5f}\n" for loss in losses)
+            with open(f"runs/{dir_name}/output/epoch_{epoch}_test_psnr.txt", "w") as f:
+                f.write(f"Epoch {epoch}\n")
+                f.writelines(f"{psnr.item():.2f}\n" for psnr in psnr_list)
+
+    # Average over batches
+    rank_avg_loss = total_loss / len(data_loader)
+    rank_avg_psnr = total_psnr / len(data_loader)
+
+    print(f"Rank {rank}, Epoch {epoch} | Avg Epoch Test loss: {rank_avg_loss:.5f} | Avg Epoch Test PSNR: {rank_avg_psnr} ")
+    return rank_avg_loss, rank_avg_psnr
+
+def compute_psnr(pred, target, clean, max_val=1.0):
     mse = torch.mean((pred - target) ** 2)
     mse = torch.clamp(mse, min=1e-10) # prevent log(infinity) issues
     return 10 * torch.log10(max_val**2 / mse)
