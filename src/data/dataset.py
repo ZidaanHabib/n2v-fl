3dd7ff5ad48237975fe8611eb39a83822ca53d36
Zidaan <zizouhabib@gmail.com>
2025-07-30 19:36:08 +0100

    feat: add dataset for synthetic noise

--- src/data/dataset.py
+++ src/data/dataset.py
@@ -32,4 +32,29 @@ class N2NImageDataset(Dataset):
         image_patch = F.crop(image, top, left, height, width)
         target_patch = F.crop(target, top, left, height, width)
         return image_patch, target_patch
+    
 
+# define custom image dataset for Synthetic data n2n
+class N2NSyntheticImageDataset(Dataset):
+    def __init__(self, data_dir: Path, dataset: str, subdataset: str, transform, patch_size: int, patches_per_image: int):
+        super().__init__()
+        dataset_dir = data_dir / dataset
+        self.subdataset = subdataset
+        self.image_dir = dataset_dir / self.subdataset # treat lowsnr as input image
+        self.transform = transform
+        self.patch_size = patch_size
+        self.patches_per_image = patches_per_image
+    
+    def __len__(self):
+        return sum(self.patches_per_image for path in self.image_dir.iterdir() if path.is_file()) # count number of images in directory
+    
+    def __getitem__(self, index):
+        img_index = (index // self.patches_per_image )
+        image = self.transform(io.decode_image(self.image_dir / f"{self.subdataset}-noisy-first-{img_index}.png", mode=io.ImageReadMode.GRAY))
+        target = self.transform(io.decode_image(self.image_dir/ f"{self.subdataset}-noisy-second-{img_index}.png", mode=io.ImageReadMode.GRAY))
+        clean = self.transform(io.decode_image(self.image_dir/ f"{self.subdataset}-clean-{img_index}.png", mode=io.ImageReadMode.GRAY))
+        top, left, height, width = v2.RandomCrop(self.patch_size).get_params(image,(self.patch_size,self.patch_size))
+        image_patch = F.crop(image, top, left, height, width)
+        target_patch = F.crop(target, top, left, height, width)
+        clean_patch = F.crop(clean, top, left, height, width)
+        return image_patch, target_patch, clean_patch
