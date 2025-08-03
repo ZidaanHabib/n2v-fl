44b48b2f8786c4b844e9bf282cb1b667406415c5
Zidaan <zizouhabib@gmail.com>
2025-08-03 12:58:55 +0100

    feat: test

--- src/benchmark_n2v.py
+++ src/benchmark_n2v.py
@@ -60,6 +60,7 @@ def main(cfg: DictConfig):
     for batch_size in batch_sizes: 
         for (patch_size, num_patches) in zip(patch_sizes, patches_per_image):
             train_loader, test_loader = load_distributed_dataset(world_size, rank, data_dir, batch_size, 16, patch_size, num_patches, has_ground_truth=False)
+            print("Dataset loadded")
 
             running_loss = torch.tensor(0, dtype=torch.float32, device=device, requires_grad=False)
             losses = torch.zeros(len(train_loader),device=device,requires_grad=False) 
